27ac07b4450d9ccb1948c1b4bc6349d162a11eea
ishi12121 <isht1402@gmail.com>
2023-03-09 19:28:30 +0530

    refactored the code and made login page frontend

---
+++ frontend/src/pages/login/index.js
@@ -0,0 +1,15 @@
+import "./style.css";
+import LoginForm from "../../components/login/LoginForm";
+import Footer from "../../components/login/Footer";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="login_wrapper">
+        <LoginForm />
+        <div className="register"></div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
