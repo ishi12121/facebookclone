0ce24e2428705c84aedfa6d5737ce59e3868053f
ishi12121 <isht1402@gmail.com>
2023-06-02 16:34:23 +0530

    create post section done

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -5,6 +5,7 @@ import LeftHome from "../../components/home/left";
 import RightHome from "../../components/home/right";
 import Stories from "../../components/home/stories";
 import "./style.css";
+import CreatePost from "../../components/createPost";
 export default function Home() {
   const { user } = useSelector((user) => ({ ...user }));
   return (
@@ -12,7 +13,9 @@ export default function Home() {
       <Header />
       <LeftHome user={user} />
       <div className="home_middle">
+      
       <Stories />
+      <CreatePost user={user} />
       </div>
       <RightHome user={user} />
 
