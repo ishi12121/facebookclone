4ee36314053c86baa275e194af1bfcbdddc50c29
ishi12121 <isht1402@gmail.com>
2023-11-10 16:54:15 +0530

    fixed an issue in cors fir cyclic

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(
   fileUpload({
     useTempFiles: true,
